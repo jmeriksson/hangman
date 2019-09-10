df9c1508e64b5519be564d1bbd956bb4f131aeea
Mikael Eriksson <mikael.eriksson.nkpg@gmail.com>
2019-09-10 10:27:13 +0200

    Adding form.

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
 
-echo "hello world....!";
+    $newSession = session_start();
+
+    if ($newSession) {
+        echo 'Started session';
+    } else {
+        echo 'failed';
+    }
+
+    $_SESSION["usedLetters"] = array();
+
+    $letterInput = $_GET[letter];
+
+    // $usedLetters = array();
+
+    if($letterInput) {
+        array_push($_SESSION["usedLetters"], $letterInput);
+    };
+
+    print_r($_SESSION["usedLetters"]);
+
+    echo "<form method='GET'>
+        <label>Pick a letter:
+            <input type='text' name='letter'></label>
+        <input type='submit' value='send'>
+    </form>";
